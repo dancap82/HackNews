7301bc1144e8f8b1011978c899a63da87cf7914f
hamed mohsen <raahnaward@gmail.com>
2024-05-02 18:28:21 +0200

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
     
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+
+
+function Footer() {
+    return (
+      <footer>
+        <div className="container">
+          <ul className="list-unstyled d-flex justify-content-center align-items-center p-0">
+            <li className="m-2 mx-3"><a href="#" className="text-decoration-none">Guidelines</a></li>
+            <li className="m-2 mx-3"><a href="#" className="text-decoration-none">FAQ</a></li>
+            <li className="m-2 mx-3"><a href="#" className="text-decoration-none">List</a></li>
+            <li className="m-2 mx-3"><a href="#" className="text-decoration-none">API</a></li>
+            <li className="m-2 mx-3"><a href="#" className="text-decoration-none">Security</a></li>
+            <li className="m-2 mx-3"><a href="#" className="text-decoration-none">Legal</a></li>
+            <li className="m-2 mx-3"><a href="#" className="text-decoration-none">Apply to YC</a></li>
+            <li className="m-2 mx-3"><a href="#" className="text-decoration-none">Contact</a></li>
+          </ul>
+        </div>
+      </footer>
+    );
+  }
+  
+export default Footer
