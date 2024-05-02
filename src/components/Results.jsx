6490bd4a02eb7adfa30d3e8b6b4191084bf0075a
Daniele <daniele.capa@gmail.com>
2024-05-02 17:12:37 +0200

    results done

---
+++ src/components/Results.jsx
@@ -0,0 +1,49 @@
+function Results() {
+    return (
+        <div className="container-fluid mt-3 h-50 d-flex justify-content-center align-items-center">
+            <div className="border border-dark bg-secondary" style={{ width: '75%', height: '75%' }}>
+                <ul className="list-group">
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 1:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 2:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 3:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 1:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 2:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 3:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 1:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 2:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 3:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 1:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 2:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                    <li className="list-group-item" style={{ paddingTop: '5px', paddingLeft: '5px' }}> {/* Added padding */}
+                        <strong>Title 3:</strong> <span style={{ marginLeft: '5px' }}>https://example.com</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      );
+}
+
+
+export default Results
