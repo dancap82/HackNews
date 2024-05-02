2aa33dd888a79ba3a65af9f973761dd55cf5ad71
hamed mohsen <raahnaward@gmail.com>
2024-05-02 18:42:59 +0200

    added some styling to footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import Results from './components/Results.jsx'
 
 
 function App() {
   return (
     <div>
       <Header />
+      <Results />
       <Footer />
     </div>
     
