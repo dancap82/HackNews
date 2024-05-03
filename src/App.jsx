eae6b284d123fe47c678f9bbb4e09f039f458818
roycejohn <cuevasroycejohn.com>
2024-05-03 21:14:43 +0200

    Adding fetch and search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/Header.jsx'
-import Results from './components/Results.jsx'
+import OneResult from './components/OneResult.jsx'
 import Footer from './components/Footer.jsx'
+import { useState, useEffect } from 'react'
 
 
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+      }, [query]
+    );
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://hn.algolia.com/api/v1/search?query=${query}`);
+      const data = await response.json();
+      setResults(data.hits);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+
   return (
     <div>
-      <Header />
-      <Results />
+      <Header query={query} setQuery={setQuery} handleSearch={handleSearch} />
+      <OneResult results={results} />
       <Footer />
     </div>
     
@@ -18,3 +43,7 @@ function App() {
 }
 
 export default App
+
+
+
+
