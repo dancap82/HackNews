e4e1f2030712862e5f6031905c29b0f066a75845
hamed mohsen <raahnaward@gmail.com>
2024-05-03 15:48:52 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/Header.jsx'
-<<<<<<< HEAD
 import Results from './components/Results.jsx'
 import Footer from './components/Footer.jsx'
-=======
-import Footer from './components/Footer.jsx'
-import Results from './components/Results.jsx'
->>>>>>> feature/HM
+
 
 
 function App() {
