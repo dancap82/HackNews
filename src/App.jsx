a3666efe2ff18b626f64d1743ab6d85a5109742a
hamed mohsen <raahnaward@gmail.com>
2024-05-06 15:48:16 +0200

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import { useEffect, useState } from 'react'
 import Header from './components/Header.jsx'
 import Results from './components/Results.jsx'
-import Footer from './components/Footer.jsx'
-
 
 
 function App() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      try {
+        const response = await fetch('https://hn.algolia.com/api/v1/search?query=React');
+        const data = await response.json();
+        setResults(data.hits);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchResults();
+  }, []); // Empty dependency array ensures the effect runs only once
+
   return (
     <div>
       <Header />
-      <Results />
-      <Footer />
+      <Results results={results}/>
     </div>
     
   )
