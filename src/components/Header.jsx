c315f29aa6e11dd126ec3cac73e2443f55766876
hamed mohsen <raahnaward@gmail.com>
2024-05-06 16:25:45 +0200

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,32 @@
 
-
-const Header = () => {
-  
-
+function Header() {
   return (
     <header className="navbar navbar-expand-lg">
       <div className="container">
         <a className="navbar-brand text-white fw-bold" href="#">WD-50 Hacker News</a>
-        <form className="form-inline ml-auto d-flex justify-content-center"  onSubmit=''>
+        <form className="form-inline ml-auto d-flex justify-content-center">
           <div className="search-bar">
             <input
               className="form-control mr-sm-2 px-5" 
               type="text"
-              // value={}
-              // onChange={}
+              value=''
+              onChange=''
               placeholder="Search stories by title, url or author"
             />
           </div>
-         <div className="input-group-append">
-              <button className="mx-1 btn btn-outline-success text-white" type="submit">
+        <div className="input-group-append">
+              <button className="mx-1 btn btn-outline-success text-white" onClick=''>
                 <i className="fas fa-search"></i>
               </button>
           </div>
         </form>
       </div>
-      
-    </header>
+    
+  </header>
+
+  
   );
-};
+}
 
 export default Header;
+
