d0a9671a045b89ad4329bf4fcc79e3c5d0e7caeb
hamed mohsen <raahnaward@gmail.com>
2024-05-07 10:32:44 +0200

    Final commit

--- src/components/OneResult.jsx
+++ src/components/OneResult.jsx
@@ -1,24 +1,24 @@
 function OneResult({ results }) {
-    return (
-      <div className="container mx-6 my-4">
-        <ol>
-          {results.map ((result) =>(
-            <li key={result.objectID}>
-              <span style={{ fontFamily: 'Arial, serif', fontSize: '14px', color: 'black' }}>{result.title} - </span>
-              <a href={result.url} 
-              target="_blank" rel="noopener noreferrer" 
+  return (
+    <div className="container mx-6 my-4">
+      <ol>
+        {results.map ((result) =>(
+          <li key={result.objectID}>
+            <span style={{ fontWeight: 'bold', fontFamily: 'Arial, serif', fontSize: '15px', color: 'black' }}>{result.title} - </span>
+            <a href={result.url}
+              target="_blank" rel="noopener noreferrer"
               className="ml-2"
-              style={{ fontFamily: 'Arial, serif', fontSize: '12px', color: 'grey' }}>{result.url}
-              
-              </a>
-           </li>
-          ))}
-        </ol>
-      </div>
-      
-    );
-  }
-
+              style={{ fontFamily: 'Arial, serif', fontSize: '15px', color: 'grey' }}>{result.url}
+            </a>
+            <div>
+              <span style={{  fontFamily: 'Arial, serif', fontSize: '14px', color: 'grey' }}> Author: {result.author} | Date created ({result.created_at})
+              </span>
+            </div>
+         </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
 export default OneResult;
-
 
